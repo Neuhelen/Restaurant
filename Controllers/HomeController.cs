10f092672f24eb1ba5695eb67a4596e508d35a66
Patrick Dahl <patrick.v.d@hotmail.com>
2023-09-27 16:53:22 +0200

    Added booking form.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,12 @@ namespace Resturant.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult BookingForm()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
