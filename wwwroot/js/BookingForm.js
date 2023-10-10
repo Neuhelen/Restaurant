c5fcdefa09808d9b51264d80902d6cde9423df12
Patrick Dahl <patrick.v.d@hotmail.com>
2023-10-10 20:38:21 +0200

    The validation works now.

--- wwwroot/js/BookingForm.js
+++ wwwroot/js/BookingForm.js
@@ -12,34 +12,51 @@ continueButton.disabled = true;
 function enableContinueButton() {
 	let enableCounter = 0;
 	for (let i = 0; i < enableContinueArray.length; i++) {
-    		if (enableContinueArray[i] == 1){
+		if (enableContinueArray[i] == 1) {
 			enableCounter += 1;
-        	} else {
+		} else {
 			continueButton.disabled = true;
-        }
+		}
 	}
-	if (enableCounter == 5){
+	if (enableCounter == 5) {
 		continueButton.disabled = false;
-    }	
+	}
 }
 
+
 function checkDate() {
-	let date = document.getElementById("bookingDate");
+
+	let dateInput = document.getElementById("bookingDate");
+
+	dateInput.style.borderColor = "red";
+
 	var isConditionMet = true;
 
-	if (!date.value || isNaN(Date.parse(date.value))) {
+	//This part parses the input date as a Date object. 
+	let selectedDate = new Date(dateInput.value);
+
+	//If the input is empty or an invalid date, the selected date is unacceptable and the user is unable to continue. 
+	if (!dateInput.value || isNaN(selectedDate)) {
 		isConditionMet = false;
 	}
 
-	var now = new Date();
-	now.setHours(0, 0, 0, 0);
-	date.setHours(0, 0, 0, 0);
-	if (date < now) {
+	//This part gets the current date. 
+	let currentDate = new Date();
+	currentDate.setHours(0, 0, 0, 0);
+
+	//If the selected is earlier than the current date, the selected is once again unacceptable. 
+	if (selectedDate < currentDate) {
 		isConditionMet = false;
 	}
 
+	//If all the criteria are met, this part passes the criteria to enable the continueButton. 
 	if (isConditionMet) {
+		dateInput.style.borderColor = "green";
 		enableContinueArray[0] = 1;
+		enableContinueButton();
+		return;
+	} else {
+		enableContinueArray[0] = 0;
 		enableContinueButton();
 		return;
 	}
@@ -51,19 +68,19 @@ function checkName() {
 	//When a button is pressed in the textfield, the border becomes red, 
 	//unless there is text in the textfield. 
 
-	document.getElementById("nameTextbox").style.borderColor = "red";
-    
-	if (textbox.value.length > 0) {
-		document.getElementById("nameTextbox").style.borderColor = "green";
+	textbox.style.borderColor = "red";
 
-		//This part activates the "Continue"-button if all the textfields fulfill their requirements. 
+	if (textbox.value.length > 0) {
+		textbox.style.borderColor = "green";
+
+		//This part activates the "Continue"-button if all the textfields and the selected date fulfill their requirements. 
 		enableContinueArray[1] = 1;
 		enableContinueButton();
 
-		return; 
-    }
+		return;
+	}
 	enableContinueArray[1] = 0;
-	enableContinueButton(); 
+	enableContinueButton();
 }
 
 function checkEmail() {
@@ -71,50 +88,43 @@ function checkEmail() {
 
 	//When a button is pressed in the textfield, the border becomes red, 
 	//unless the text in the textfield fullfils all the conditions in the if-statements. 
+	textbox.style.borderColor = "red";
 
-	document.getElementById("emailTextbox").style.borderColor = "red";
-
-	//These if-statements check whether the text in the textfield passes our criteria for an email. 
-	//There must be 2 characters before the "@@", 2 more characters, a "." and finally 2 more characters 
-	//in the textfield to pass the if-statements and make the border of the textfield green. 
-
+	//These if-statements check whether the text in the textfield passes our criteria for an email.
 	var isConditionMet = true;
 
-½	//If the text in the textfield is less than 8 characters in length, the format is not unacceptable by our parameters. 
+	//If the text in the textfield is less than 8 characters in length, the format is unacceptable by our parameters.
 	if (textbox.value.length < 8) {
 		isConditionMet = false;
 	}
 
-	//If the text in the textfield doesn't include an "@", the text is not an email and thus unacceptable. 
+	//If the text in the textfield doesn't include an "@", the text is not an email and thus unacceptable.
 	if (!textbox.value.includes("@")) {
 		isConditionMet = false;
 	}
 
-	//If there is no text before the "@", the text is not an email and thus unacceptable. 
+	//If there is no text before the "@", the text is not an email and thus unacceptable.
 	if (textbox.value.substring(0, textbox.value.indexOf("@")).length < 2) {
 		isConditionMet = false;
 	}
 
-	//If the text in the textfield doesn't include an "." after the "@", the text is not an email and thus unacceptable. 
-	if (!textbox.value.substring(textbox.value.indexOf("@@")).includes(".")) {
+	//If the text in the textfield doesn't include a "." after the "@", the text is not an email and thus unacceptable.
+	if (!textbox.value.substring(textbox.value.indexOf("@")).includes(".")) {
 		isConditionMet = false;
 	}
 
-	//If the in the textfield doesn't include 2 characters after the "@", but before the last ".", we don't accept it. 
-	if (textbox.value.substring(textbox.value.indexOf("@@") + 1, textbox.value.lastIndexOf(".")).length < 2) {
-		isConditionMet = false;
-	}
-
-	//If the in the textfield doesn't include 2 characters after the last ".", we don't accept it. 
+	//If the text in the textfield doesn't include 2 characters after the last ".", we don't accept it.
 	if (textbox.value.substring(textbox.value.lastIndexOf(".") + 1).length < 2) {
 		isConditionMet = false;
 	}
 
+	//If all the text in the textfield passes all the if-statements, we accept the text as an email
+	//this part passes the criteria to enable the continueButton.
 	if (isConditionMet) {
-		document.getElementById("emailTextbox").style.borderColor = "green";
+		textbox.style.borderColor = "green";
 		enableContinueArray[2] = 1;
 		enableContinueButton();
-	} 
+	}
 }
 
 function checkPhoneNumber() {
@@ -122,37 +132,37 @@ function checkPhoneNumber() {
 	//If there is text in the the textbox that includes " ", as well as text before and after the " ", 
 	//the box fulfills one of the 5 requirements to continue.
 	let textbox = document.getElementById("phoneTextbox");
-	document.getElementById("phoneTextbox").style.borderColor = "red";
+	textbox.style.borderColor = "red";
 
 	if (textbox.value.length == 8) {
-		document.getElementById("phoneTextbox").style.borderColor = "green";
+		textbox.style.borderColor = "green";
 
-		//This part activates the "Continue"-button if all the textfields fulfill their requirements. 
+		//This part activates the "Continue"-button if all the textfields and the selected date fulfill their requirements. 
 		enableContinueArray[3] = 1;
 		enableContinueButton();
 
 		return;
-    }
+	}
 	enableContinueArray[3] = 0;
-	enableContinueButton(); 
+	enableContinueButton();
 }
 
 function checkParty() {
 	let textbox = document.getElementById("partyTextbox");
 
-	document.getElementById("partyTextbox").style.borderColor = "red";
-    
-	if (textbox.value.length > 0) {
-		document.getElementById("partyTextbox").style.borderColor = "green";
+	textbox.style.borderColor = "red";
 
-		//This part activates the "Continue"-button if all the textfields fulfill their requirements. 
+	if (textbox.value.length > 0) {
+		textbox.style.borderColor = "green";
+
+		//This part activates the "Continue"-button if all the textfields and the selected date fulfill their requirements.
 		enableContinueArray[4] = 1;
 		enableContinueButton();
 
-		return; 
-    }
+		return;
+	}
 	enableContinueArray[4] = 0;
-	enableContinueButton(); 
+	enableContinueButton();
 }
 
 function checkMeals() {
@@ -161,17 +171,17 @@ function checkMeals() {
 	//When a button is pressed in the textfield, the border becomes red, 
 	//unless there is text in the textfield. 
 
-	document.getElementById("mealTextbox").style.borderColor = "red";
-    
-	if (textbox.value.length > 0) {
-		document.getElementById("mealTextbox").style.borderColor = "green";
+	textbox.style.borderColor = "red";
 
-		//This part activates the "Continue"-button if all the textfields fulfill their requirements. 
+	if (textbox.value.length > 0) {
+		textbox.style.borderColor = "green";
+
+		//This part activates the "Continue"-button if all the textfields and the selected date fulfill their requirements.
 		enableContinueArray[5] = 1;
 		enableContinueButton();
 
-		return; 
-    }
+		return;
+	}
 	enableContinueArray[5] = 0;
-	enableContinueButton(); 
+	enableContinueButton();
 }
