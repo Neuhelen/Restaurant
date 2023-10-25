79ae4d97b7987aa5f60e373ae610647b2b544604
Patrick Dahl <patrick.v.d@hotmail.com>
2023-10-25 11:37:00 +0200

    Basic menu with a terrible layout and a message that changes based on the time of the day. I will make the menu nicer to look at and make some code to hook up the menu with our database.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,8 +33,13 @@ namespace Resturant.Controllers
 			return View();
 		}
 
+        public IActionResult Menu()
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
