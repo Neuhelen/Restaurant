2a150c5b2c8e5db3df27ec373df15cbf6b0f87a3
DustLotus <harryzhang331@gmail.com>
2023-11-29 10:26:24 +0100

    BookingDone Test2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -48,11 +48,6 @@ namespace Resturant.Controllers
 			return View();
 		}
 
-        public IActionResult Menu()
-        {
-            return View();
-        }
-
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
